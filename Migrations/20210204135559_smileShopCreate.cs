4d29a73d8a737e498e9e173e722fa3779e48223d
ko <go.nobpadon25@gmail.com>
2021-02-06 14:11:53 +0700

    *

---
+++ Migrations/20210204135559_smileShopCreate.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCoreAPI_Template_v3_with_auth.Migrations
+{
+    public partial class smileShopCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "auth");
+
+            migrationBuilder.CreateTable(
+                name: "ProductGroup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductGroup", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                schema: "auth",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "auth",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Username = table.Column<string>(maxLength: 20, nullable: false),
+                    PasswordHash = table.Column<byte[]>(nullable: false),
+                    PasswordSalt = table.Column<byte[]>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    StockCount = table.Column<int>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ProductGroupId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_ProductGroup_ProductGroupId",
+                        column: x => x.ProductGroupId,
+                        principalTable: "ProductGroup",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRole",
+                schema: "auth",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    RoleId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRole", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_UserRole_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "auth",
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRole_User_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "auth",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "auth",
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("62a0d233-610d-45d2-8fa8-daa6bec4dd42"), "user" },
+                    { new Guid("42f106fb-a798-4483-80bf-2dc9c09dbde0"), "Manager" },
+                    { new Guid("4ec3db99-2fec-433c-bb7f-aa67fe5b6af7"), "Admin" },
+                    { new Guid("57fb82f6-9bc6-4f6c-b581-e67fd0549ba5"), "Developer" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ProductGroupId",
+                table: "Product",
+                column: "ProductGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRole_RoleId",
+                schema: "auth",
+                table: "UserRole",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "UserRole",
+                schema: "auth");
+
+            migrationBuilder.DropTable(
+                name: "ProductGroup");
+
+            migrationBuilder.DropTable(
+                name: "Role",
+                schema: "auth");
+
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "auth");
+        }
+    }
+}
---
+++ Migrations/20210206063137_OrderCreate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCoreAPI_Template_v3_with_auth.Migrations
+{
+    public partial class OrderCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "auth",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("42f106fb-a798-4483-80bf-2dc9c09dbde0"));
+
+            migrationBuilder.DeleteData(
+                schema: "auth",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("4ec3db99-2fec-433c-bb7f-aa67fe5b6af7"));
+
+            migrationBuilder.DeleteData(
+                schema: "auth",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("57fb82f6-9bc6-4f6c-b581-e67fd0549ba5"));
+
+            migrationBuilder.DeleteData(
+                schema: "auth",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("62a0d233-610d-45d2-8fa8-daa6bec4dd42"));
+
+            migrationBuilder.InsertData(
+                schema: "auth",
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("6200d70b-07c2-4df7-a6b4-77782174bfec"), "user" },
+                    { new Guid("3793bccb-ea45-4eb4-876b-c272b9ac4931"), "Manager" },
+                    { new Guid("59e892f0-19cc-4ff7-ad63-5d0d0ec574ff"), "Admin" },
+                    { new Guid("1eb719bb-fe61-426f-86e0-495d6c349816"), "Developer" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "auth",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("1eb719bb-fe61-426f-86e0-495d6c349816"));
+
+            migrationBuilder.DeleteData(
+                schema: "auth",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("3793bccb-ea45-4eb4-876b-c272b9ac4931"));
+
+            migrationBuilder.DeleteData(
+                schema: "auth",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("59e892f0-19cc-4ff7-ad63-5d0d0ec574ff"));
+
+            migrationBuilder.DeleteData(
+                schema: "auth",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("6200d70b-07c2-4df7-a6b4-77782174bfec"));
+
+            migrationBuilder.InsertData(
+                schema: "auth",
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("62a0d233-610d-45d2-8fa8-daa6bec4dd42"), "user" },
+                    { new Guid("42f106fb-a798-4483-80bf-2dc9c09dbde0"), "Manager" },
+                    { new Guid("4ec3db99-2fec-433c-bb7f-aa67fe5b6af7"), "Admin" },
+                    { new Guid("57fb82f6-9bc6-4f6c-b581-e67fd0549ba5"), "Developer" }
+                });
+        }
+    }
+}
